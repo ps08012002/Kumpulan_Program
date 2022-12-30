4dcf88c5c5d179145707ff8781b8a464fbda10c0
Putra_Sangaji <ps08012002@gmail.com>
2022-12-30 14:16:53 +0700

    Proses Mencari Bilangan Prima Dalam Range

--- cobalagi.py
+++
@@ -1,1 +0,0 @@
-print ("apaan tuh")
--- test.py
+++ test.py
@@ -1,39 +1,32 @@
-# # awal = int(input("Masukkan Angka Awal : "))
-# # akhir = int(input("Masukkan Angka Akhir : "))
-
-a = int(input("Masukkan Bilangan Yang Akan Dicek : "))
+awal = int(input("Masukkan Angka Awal : "))
+akhir = int(input("Masukkan Angka Akhir : "))
 
 i = 1
 c = 0
+prima = []
+a = int(range(awal,akhir))
 
-while i <= a :
+print (type(a))
+# print (70*"=" + "\n")
+# a = int(input("Masukkan Bilangan Yang Akan Dicek : "))
+# print (70*"=" + "\n")
+
+while i in range (awal, akhir) :
     b = a%i
     i += 1
-
     if b == 0 :
-        
         c += 1
-
     if i > a :
         break
+        
+    # if c == 2 :
+    #     print(f"{a} Adalah Bilangan Prima")
+    # else :
+    #     print(f"{a} Bukanlah Bilangan Prima")
 
-if c == 2 :
-    print("Ini Bilangan Prima")
 
-else :
-    print("Bukan Bilanagn Prima")
+for i in range (awal, akhir +1) :
+    if c == 2 :
+        prima.append(a)
 
-# z = 7 % 4
-# print (z)
-# # if a%1 == 0 and a%a == 0 :
-# #     print ("ini bilangan prima")
-
-# # else :
-# #     print ("ini bukan bilangan prima")
-
-# a=1
-# b=7
-
-# c= b%a
-
-# print(type(c))
+print (prima)
